0e39e4a78ff201eb508a8bd1d571114031fa58a6
panchoman32 <estebandido32@hotmail.com>
2022-12-13 22:11:33 -0700

    add usecases for user

---
+++ src/usecases/users/index.js
@@ -0,0 +1,49 @@
+const Users = require("../../models/user").model;
+const { hashPassword, verifyPass } = require("../../lib/encrypt");
+const { createToken } = require("../../lib/jwt");
+
+const create = async (data) => {
+  const { userName, password, email } = data;
+
+  const passwordHashed = await hashPassword(password);
+  const user = new Users({ userName, passwordHashed, email });
+  return await user.save();
+};
+
+const getUserById = async (id) => await Users.findById(id).exec();
+
+const getAllUsers = async () => {
+  return await Users.find({}).exec();
+};
+
+const updateUser = async (id, data) => {
+  const { userName, password, email } = data;
+
+  data.userName = userName ? userName : data.userName;
+  data.password = password ? password : data.password;
+  data.email = email ? email : data.email;
+  return await Users.findByIdAndUpdate(id, data).exec();
+};
+
+const findByEmail = async (email) => await Users.findOne({ email });
+
+const authenticate = async (email, password) => {
+  const user = await findByEmail(email);
+  const hash = user.password;
+
+  const isVerified = await verifyPass(password, hash);
+  if (!isVerified) throw new Error("nel prro, escribe bien el pass");
+  return createToken({ sub: user._id });
+};
+
+const del = async (id) => await Users.findByIdAndDelete(id).exec();
+
+module.exports = {
+  create,
+  getUserById,
+  getAllUsers,
+  updateUser,
+  findByEmail,
+  authenticate,
+  del,
+};
