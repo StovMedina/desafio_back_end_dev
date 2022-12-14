dd9a5cfc08a76c44197f550ec37e9c1fe1d7cc9e
Sof <sofie151091@gmail.com>
2022-12-14 00:10:58 -0600

    method get/post added

---
+++ src/routes/usersRoutes.js
@@ -0,0 +1,63 @@
+const { Router } = require("express");
+const { getAll, create, authenticate, getById } = require("../usecases/user");
+const { authHandler } = require("../middlewares/authHandler");
+
+const routes = Router();
+
+routes.get("/", authHandler, async (req, res) => {
+  const id = req.params.token.sub;
+
+  const { userNAme, passwordHashed, email } = await getById(id);
+
+  res.json({ ok: true, payload: { userNAme, passwordHashed, email } 
+});
+});
+
+routes.post("/", async (req, res) => {
+  const { userNAme, passwordHashed } = req.body;
+
+  try {
+    const payload = await create({ userNAme, passwordHashed,});
+    res.json({ ok: true, payload });
+  } catch (error) {
+    const { message } = error;
+    res.status(400).json({ ok: false, message });
+  }
+});
+
+routes.post("/auth", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const payload = await authenticate(email, password);
+    res.status(202).json({ ok: true, payload });
+  } catch (error) {
+    const { message } = error;
+    res.status(401).json({ ok: false, message });
+  }
+});
+
+routes.put("/", async (req, res) => {
+    
+
+    try {
+        
+    } catch (error) {
+        
+    }
+//   res.status(405).json({ message: "Method not allowed" });
+});
+
+routes.put("/:id", (req, res) => {
+  // Lógica para editar el usuario con el id X
+
+  res.json({ message: `Usuario con el id ${req.params.id} modificado` });
+});
+
+routes.delete("/:id", (req, res) => {
+  // Logica para eliminar el usuario con el id X
+
+  res.json({ message: `Usuario con el id ${req.params.id} eliminado` });
+});
+
+module.exports = routes;
